9246d2bc4a762edb830640e9a50ff0d8a05e424d
Zuppah1 <mantvydas.rinkunas@gmail.com>
2025-11-18 18:59:32 +0200

    added caching

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -13,6 +13,16 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		url = *pageURL
 	}
 
+	if val, ok := c.cache.Get(url); ok {
+		locationResp := RespShallowLocations{}
+		err := json.Unmarshal(val, &locationResp)
+		if err != nil {
+			return RespShallowLocations{}, err
+		}
+
+		return locationResp, nil
+	}
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return RespShallowLocations{}, err
@@ -37,6 +47,8 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		return RespShallowLocations{}, nil
 
 	}
+
+	c.cache.Add(url, dat)
 
 	return locationResp, nil
 }
